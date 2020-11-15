46a5a6ebd48b189c43597d1e7c5a688899fbe69c
wangsimiao <wangsimiao.tjpu@bytedance.com>
2020-11-15 22:25:54 +0800

    feat: 修改目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,40 @@
 module.exports = {
   // theme: 'reco',
-  title:'Geronimo 邈 ',
-  description: '种一棵树最好的时间是十年前 其次是现在',
-  base: '/',
+  title: "Geronimo 邈 ",
+  description: "种一棵树最好的时间是十年前 其次是现在",
+  base: "/",
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+    ["link", { rel: "icon", href: "/favicon.ico" }],
+    [
+      "meta",
+      {
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,user-scalable=no",
+      },
+    ],
   ],
   themeConfig: {
-      nav: [
-        { text: 'GitHub', link: 'https://github.com/Geronimomiao', icon: 'reco-github' },
-      ],
-     
-        search: true,
-        searchMaxSuggestions: 10,
-        sidebar: 'auto',
-        lastUpdated: 'Last Updated',
-        author: 'Geronimo 邈',
-        record: '豫ICP备18026088号',
-        startYear: '2019',
-        logo: '/yuan.png',
+    nav: [
+      {
+        text: "GitHub",
+        link: "https://github.com/Geronimomiao",
+        icon: "reco-github",
+      },
+    ],
+
+    search: true,
+    searchMaxSuggestions: 10,
+    sidebar: "auto",
+    lastUpdated: "Last Updated",
+    author: "Geronimo 邈",
+    record: "豫ICP备18026088号",
+    startYear: "2019",
+    logo: "/yuan.png",
   },
-  plugins: ['permalink-pinyin', ['autobar', {'pinyinNav': true}], 'rpurl']
-}
+  plugins: [
+    "permalink-pinyin",
+    ["autobar", { pinyinNav: true }],
+    "rpurl",
+    "meting",
+  ],
+};
